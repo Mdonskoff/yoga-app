c5cd7065223e3eff8ae02e8bc3587b1de97ba7f0
Marushka Donskoff <marushkoff@gmail.com>
2024-04-21 20:51:12 +0200

    Update controllers tests

--- back/src/test/java/com/openclassrooms/starterjwt/controllers/UserControllerIT.java
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/UserControllerIT.java
@@ -1,8 +1,10 @@
 package com.openclassrooms.starterjwt.controllers;
 
+import com.openclassrooms.starterjwt.dto.UserDto;
 import com.openclassrooms.starterjwt.mapper.UserMapper;
 import com.openclassrooms.starterjwt.models.User;
 import com.openclassrooms.starterjwt.repository.UserRepository;
+import com.openclassrooms.starterjwt.security.services.UserDetailsImpl;
 import com.openclassrooms.starterjwt.services.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -13,8 +15,17 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
@@ -51,5 +62,109 @@ public class UserControllerIT {
         user.setEmail("test@test.com");
         user.setLastName("test");
         user.setFirstName("test");
+
+        UserDto expectedUserDto = userMapper.toDto(user);
+        when(userRepository.findById(Long.parseLong(id))).thenReturn(Optional.of(user));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/user/{id}", id))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email").value(expectedUserDto.getEmail()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.lastName").value(expectedUserDto.getLastName()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value(expectedUserDto.getFirstName()));
+
+        verify(userRepository).findById(Long.parseLong(id));
     }
+
+    @Test
+    @DisplayName("Should not found user")
+    void giveIdUser_thenFindUserById_shouldNotFoundUser() throws Exception {
+
+        when(userRepository.findById(Long.parseLong(id))).thenReturn(Optional.empty());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/user/{id}", id))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+
+        verify(userRepository).findById(Long.parseLong(id));
+    }
+    @Test
+    @DisplayName("Should throw a bad request error")
+    void giveIdUser_thenFindUserById_shouldThrowBadRequestError() throws Exception {
+
+        when(userRepository.findById(Long.parseLong(id))).thenThrow(NumberFormatException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/user/{id}", id))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+
+        verify(userRepository).findById(Long.parseLong(id));
+    }
+
+    @Test
+    @DisplayName("Should delete a user")
+    void giveIdUser_thenFindUserById_shouldDeleteUser() throws Exception {
+
+        User user = new User();
+        user.setEmail("test@test.com");
+        UserDetailsImpl userDetails = new UserDetailsImpl(1L, "test@test.com", "John",
+                "Doe", false, "password");
+        SecurityContext securityContext = mock(SecurityContext.class);
+        SecurityContextHolder.setContext(securityContext);
+
+        when(securityContext.getAuthentication()).thenReturn(new UsernamePasswordAuthenticationToken(userDetails, null));
+        when(userRepository.findById(Long.parseLong(id))).thenReturn(Optional.of(user));
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/user/{id}", id))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+        verify(userRepository).findById(Long.parseLong(id));
+    }
+
+    @Test
+    @DisplayName("Should return a not found status")
+    void giveIdUser_thenFindUserById_shouldReturnNotFindStatus() throws Exception {
+
+        when(userRepository.findById(Long.parseLong(id))).thenReturn(Optional.empty());
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/user/{id}", id))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+
+        verify(userRepository).findById(Long.parseLong(id));
+    }
+
+    @Test
+    @DisplayName("Should return a unauthorized status")
+    void giveIdUser_thenFindUserById_shouldReturnUnauthorizedStatus() throws Exception {
+
+        User user = new User();
+        user.setEmail("t@test.com");
+        UserDetailsImpl userDetails = new UserDetailsImpl(1L, "test@test.com", "John",
+                "Doe", false, "password");
+        SecurityContext securityContext = mock(SecurityContext.class);
+        SecurityContextHolder.setContext(securityContext);
+
+        when(securityContext.getAuthentication()).thenReturn(new UsernamePasswordAuthenticationToken(userDetails, null));
+        when(userRepository.findById(Long.parseLong(id))).thenReturn(Optional.of(user));
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/user/{id}", id))
+                        .andExpect(MockMvcResultMatchers.status().isUnauthorized());
+
+        verify(userRepository).findById(Long.parseLong(id));
+    }
+
+    @Test
+    @DisplayName("Should return a bad request status")
+    void giveIdUser_thenFindUserById_shouldReturnBadRequestStatus() throws Exception {
+
+        when(userRepository.findById(Long.parseLong(id))).thenThrow(NumberFormatException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/user/{id}", id))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+
+        verify(userRepository).findById(Long.parseLong(id));
+    }
+
+
+
+
+
 }
