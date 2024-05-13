97813be9a4c69505c713bddf5c530ca56f5fed5b
Marushka Donskoff <marushkoff@gmail.com>
2024-05-13 10:05:24 +0200

    Update back tests

--- back/src/test/java/com/openclassrooms/starterjwt/security/AuthTokenFilterTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/security/AuthTokenFilterTest.java
@@ -1,15 +1,43 @@
 package com.openclassrooms.starterjwt.security;
 
+import com.openclassrooms.starterjwt.controllers.AuthController;
+import com.openclassrooms.starterjwt.security.jwt.JwtUtils;
+import com.openclassrooms.starterjwt.security.services.UserDetailsImpl;
+import com.openclassrooms.starterjwt.security.services.UserDetailsServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpServletRequest;
 
 import com.openclassrooms.starterjwt.security.jwt.AuthTokenFilter;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+
+import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class AuthTokenFilterTest {
+    @Mock
+    private UserDetailsServiceImpl userDetailsService;
 
-    private final AuthTokenFilter authTokenFilter = new AuthTokenFilter();
+    private JwtUtils jwtUtils;
+
+    @InjectMocks
+    private AuthTokenFilter authTokenFilter;
+
 
     @Test
     void parseJwtValidToken() {
@@ -23,6 +51,35 @@ class AuthTokenFilterTest {
 
         // Assert
         assertThat(result).isEqualTo(validToken);
+    }
+
+    /*
+    @Test
+    void InvalidJwtToken() throws ServletException, IOException {
+        Authentication authentication = mock(Authentication.class);
+        when(authentication.getPrincipal()).thenReturn(new UserDetailsImpl(1L, "test@test.com", "Bruce", "Wayne", false, "1234"));
+        String token = jwtUtils.generateJwtToken(authentication);
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.addHeader("Authorization", "Bearer " + token);
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        FilterChain filterChain = mock(FilterChain.class);
+
+        authTokenFilter.doFilterInternal(request, response, filterChain);
+
+        assertNull(SecurityContextHolder.getContext().getAuthentication());
+    }
+     */
+
+    @Test
+    void NoJwtToken() throws ServletException, IOException {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        FilterChain filterChain = mock(FilterChain.class);
+
+        authTokenFilter.doFilterInternal(request, response, filterChain);
+
+        assertNull(SecurityContextHolder.getContext().getAuthentication());
     }
 
     @Test
--- back/src/test/java/com/openclassrooms/starterjwt/security/UserDetailsServiceImplTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/security/UserDetailsServiceImplTest.java
@@ -2,15 +2,20 @@ package com.openclassrooms.starterjwt.security;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import com.openclassrooms.starterjwt.models.User;
@@ -26,6 +31,9 @@ public class UserDetailsServiceImplTest {
 
     @InjectMocks
     private UserDetailsServiceImpl userDetailsServiceImpl;
+
+    @InjectMocks
+    UserDetailsServiceImpl userDetailsService;
 
     @Test
     void loadUserByUsernameTest() {
@@ -54,5 +62,29 @@ public class UserDetailsServiceImplTest {
         assertThatThrownBy(() -> userDetailsServiceImpl.loadUserByUsername("user1@mail.com"))
                 .isInstanceOf(UsernameNotFoundException.class)
                 .hasMessage("User Not Found with email: user1@mail.com");
+    }
+
+    @Test
+    @DisplayName("should return UserDetailsImpl object")
+    public void giveUserEmail_thenLoadUserByUsername_shouldReturnUserDetailsImplObject() {
+        userDetailsService = new UserDetailsServiceImpl(userRepository);
+
+        String email = "test@test.fr";
+        User user = new User();
+        user.setId(1L).setEmail(email).setLastName("Delore").setFirstName("Victore").setPassword("test");
+        UserDetails userDetails = UserDetailsImpl
+                .builder()
+                .id(user.getId())
+                .username(user.getEmail())
+                .lastName(user.getLastName())
+                .firstName(user.getFirstName())
+                .password(user.getPassword())
+                .build();
+
+        when(userRepository.findByEmail(email)).thenReturn(Optional.of(user));
+
+        UserDetails newUserDetail = userDetailsService.loadUserByUsername(email);
+
+        assertEquals(userDetails, newUserDetail);
     }
 }
